ec71dd4c1fab644972d3e5d683b8bef6805b5b90
William@lab <iwtfagp@gmail.com>
2015-02-28 21:41:54 +0800

    BUG

--- HumanMeasureSystem/mainwindow.cpp
+++ HumanMeasureSystem/mainwindow.cpp
@@ -6,12 +6,14 @@
 //vector<double> data_buffer[15];
 //double **data_buffer = new double[5][3];
 
-const int row_Num = 5;
-const int col_Num = 3;
-double data_buffer[row_Num][col_Num];
+//const int row_Num = 5;
+//const int col_Num = 3;
+//double data_buffer[row_Num][col_Num];
 
+const int data_buffer_number = 15+4;
+double data_buffer[data_buffer_number] = {0};
 
-
+bool dataIn = false;
 
 //data_buffer[0][0~2] is lpms_data
 //data_buffer[1][0~2] is LEFTTHIGH
@@ -70,59 +72,58 @@ void MainWindow::updateView()
 {
 
 
-    mpuLpms_back->getAngle();
+//    mpuLpms_back->getAngle();
 
-//    if (lpms_back->getConnectionStatus() == SENSOR_CONNECTION_CONNECTED) {
-//        // Reads quaternion data
-//        d = lpms_back->getCurrentData();
-////        back_ref.roll = d.r[0];
-////        back_ref.yaw = d.r[1];
-////        back_ref.pitch = d.r[2];
-//        data_buffer[0][0] = d.r[0];
-//        data_buffer[0][1] = d.r[1];
-//        data_buffer[0][2] = d.r[2];
-//    }
-
-//    mpu9150_left_thigh->getAngle(&data_buffer[LEFTTHIGH][0], &data_buffer[LEFTTHIGH][1], &data_buffer[LEFTTHIGH][2]);
-//    mpu9150_right_thigh->getAngle(&data_buffer[RIGHTTHIGH][0], &data_buffer[RIGHTTHIGH][1], &data_buffer[RIGHTTHIGH][2]);
-//    mpu9150_left_shank->getAngle(&data_buffer[LEFTSHANK][0], &data_buffer[LEFTSHANK][1], &data_buffer[LEFTSHANK][2]);
-//    mpu9150_right_shank->getAngle(&data_buffer[RIGHTSHANK][0], &data_buffer[RIGHTSHANK][1], &data_buffer[RIGHTSHANK][2]);
-        mpu9150_left_thigh->getAngle();
-        mpu9150_right_thigh->getAngle();
-        mpu9150_left_shank->getAngle();
-        mpu9150_right_shank->getAngle();
+//    mpu9150_left_thigh->getAngle();
+//    mpu9150_right_thigh->getAngle();
+//    mpu9150_left_shank->getAngle();
+//    mpu9150_right_shank->getAngle();
 
 
-//    int back = data_buffer[0][0];
-//    left_thigh = data_buffer[LEFTTHIGH][0];
-//    right_thigh = data_buffer[RIGHTTHIGH][0];
-//    left_shank = data_buffer[LEFTSHANK][0];
-//    right_shank = data_buffer[RIGHTSHANK][0];
 
-
-    ui->lcdNumber_back->display(mpuLpms_back->get_roll());
-    ui->lcdNumber_left_thigh->display(mpu9150_left_thigh->get_roll());
-    ui->lcdNumber_right_thigh->display(mpu9150_right_thigh->get_roll());
-    ui->lcdNumber_left_shank->display(mpu9150_left_shank->get_roll());
-    ui->lcdNumber_right_shank->display(mpu9150_right_shank->get_roll());
-
-
-    enum legAngle{left_hip, right_hip, left_knee, right_knee};
+    enum LEG_ANGLE{left_hip, right_hip, left_knee, right_knee};
     
+//    angle_data_buffer[left_hip] = -(mpu9150_left_thigh->get_roll() - mpuLpms_back->get_roll());
+//    angle_data_buffer[right_hip] = -(mpu9150_right_thigh->get_roll() - mpuLpms_back->get_roll());
+//    angle_data_buffer[left_knee] = mpu9150_left_shank->get_roll() - mpu9150_left_thigh->get_roll();
+//    angle_data_buffer[right_knee] = mpu9150_right_shank->get_roll() - mpu9150_right_thigh->get_roll();
 
-    angle_data_buffer[left_hip] = -(mpu9150_left_thigh->get_roll() - mpuLpms_back->get_roll());
-    angle_data_buffer[right_hip] = -(mpu9150_right_thigh->get_roll() - mpuLpms_back->get_roll());
-    angle_data_buffer[left_knee] = mpu9150_left_shank->get_roll() - mpu9150_left_thigh->get_roll();
-    angle_data_buffer[right_knee] = mpu9150_right_shank->get_roll() - mpu9150_right_thigh->get_roll();
+//    for(int i = 0; i< 4; i++)
+//    {
+//        if(angle_data_buffer[i] > 180)
+//            angle_data_buffer[i] -= 360;
+//        else if(angle_data_buffer[i] < -180)
+//            angle_data_buffer[i] +=360;
 
-    for(int i = 0; i< 4; i++)
-    {
-        if(angle_data_buffer[i] > 180)
-            angle_data_buffer[i] -= 360;
-        else if(angle_data_buffer[i] < -180)
-            angle_data_buffer[i] +=360;
+//        data_buffer[i] = angle_data_buffer[i];
+//    }
+//    data_buffer[4] = mpuLpms_back->get_roll();
+//    data_buffer[4+1] = mpuLpms_back->get_yaw();
+//    data_buffer[4+2] = mpuLpms_back->get_pitch();
 
-    }
+//    data_buffer[7] = mpu9150_left_thigh->get_roll();
+//    data_buffer[7+1] = mpu9150_left_thigh->get_yaw();
+//    data_buffer[7+2] = mpu9150_left_thigh->get_pitch();
+
+//    data_buffer[10] = mpu9150_right_thigh->get_roll();
+//    data_buffer[10+1] = mpu9150_right_thigh->get_yaw();
+//    data_buffer[10+2] = mpu9150_right_thigh->get_pitch();
+
+//    data_buffer[13] = mpu9150_left_shank->get_roll();
+//    data_buffer[13+1] = mpu9150_left_shank->get_yaw();
+//    data_buffer[13+2] = mpu9150_left_shank->get_pitch();
+
+//    data_buffer[16] = mpu9150_right_shank->get_roll();
+//    data_buffer[16+1] = mpu9150_right_shank->get_yaw();
+//    data_buffer[16+2] = mpu9150_right_shank->get_pitch();
+
+
+
+    ui->lcdNumber_back->display(data_buffer[4]);
+    ui->lcdNumber_left_thigh->display(data_buffer[7]);
+    ui->lcdNumber_right_thigh->display(data_buffer[10]);
+    ui->lcdNumber_left_shank->display(data_buffer[13]);
+    ui->lcdNumber_right_shank->display(data_buffer[16]);
 
     ui->Dial_left_hip->setNeedle(new QwtDialSimpleNeedle( QwtDialSimpleNeedle::Arrow ,true , Qt::red ));
     ui->Dial_left_hip->setValue(angle_data_buffer[left_hip]);
@@ -142,89 +143,50 @@ void MainWindow::updateView()
 }
 void MainWindow::on_pushButton_deviceConnect_clicked()
 {
+
+
+//    mpuLpms_back->ConnectIMU(ui->lineEdit_lpmsPortName->text().toLocal8Bit());
+
+//    mpu9150_left_thigh = new MPU_9150();
+//    bool b_lh  =mpu9150_left_thigh->open(ui->comboBoxIMU_left_thigh->currentText().remove("COM").toInt(), "MPU9150-1.dll");
+//    mpu9150_right_thigh = new MPU_9150();
+//    bool b_rh  =mpu9150_right_thigh->open(ui->comboBoxIMU_right_thigh->currentText().remove("COM").toInt(), "MPU9150-2.dll");
+//    mpu9150_left_shank = new MPU_9150();
+//    bool b_lk  =mpu9150_left_shank->open(ui->comboBoxIMU_left_shank->currentText().remove("COM").toInt(), "MPU9150-3.dll");
+//    mpu9150_right_shank = new MPU_9150();
+//    bool b_rk  =mpu9150_right_shank->open(ui->comboBoxIMU_right_shank->currentText().remove("COM").toInt(), "MPU9150-4.dll");
+
+
+//    QString qs_tmp;
+//    qs_tmp = "Left thigh = ";qs_tmp.append(!b_lh? "OK":"Fail");
+//    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
+//    qs_tmp = "Left shank = ";qs_tmp.append(!b_lk? "OK":"Fail");
+//    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
+//    qs_tmp = "Right thigh = ";qs_tmp.append(!b_rh? "OK":"Fail");
+//    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
+//    qs_tmp = "Right thigh = ";qs_tmp.append(!b_rk? "OK":"Fail");
+//    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
+
+//    if(!b_lh && !b_lk && !b_rh && !b_rk){
+//        cout<<"ALL device OK"<<endl;
+//        ui->textBrowser->append("ALL device OK");
+//        timer_->start();
+//    }
+
+    timer_->start();
+
+    ui->pushButton_newFile->setEnabled(true);
     ui->comboBoxIMU_left_shank->setDisabled(true);
     ui->comboBoxIMU_left_thigh->setDisabled(true);
     ui->comboBoxIMU_right_shank->setDisabled(true);
     ui->comboBoxIMU_right_thigh->setDisabled(true);
     ui->pushButton_deviceConnect->setDisabled(true);
-
-
-
-//    // Gets a LpmsSensorManager instance
-//    manager = LpmsSensorManagerFactory();
-//    // Connects to LPMS-B sensor with address 00:11:22:33:44:55
-//    lpms_back = manager->addSensor(DEVICE_LPMS_B, ui->lineEdit_lpmsPortName->text().toLocal8Bit());
-//    lpms_back->getConnectionStatus();
-    mpuLpms_back->ConnectIMU(ui->lineEdit_lpmsPortName->text().toLocal8Bit());
-
-
-    mpu9150_left_thigh = new MPU_9150();
-    bool b_lh  =mpu9150_left_thigh->open(ui->comboBoxIMU_left_thigh->currentText().remove("COM").toInt(), "MPU9150-1.dll");
-
-    mpu9150_right_thigh = new MPU_9150();
-    bool b_rh  =mpu9150_right_thigh->open(ui->comboBoxIMU_right_thigh->currentText().remove("COM").toInt(), "MPU9150-2.dll");
-
-    mpu9150_left_shank = new MPU_9150();
-    bool b_lk  =mpu9150_left_shank->open(ui->comboBoxIMU_left_shank->currentText().remove("COM").toInt(), "MPU9150-3.dll");
-
-    mpu9150_right_shank = new MPU_9150();
-    bool b_rk  =mpu9150_right_shank->open(ui->comboBoxIMU_right_shank->currentText().remove("COM").toInt(), "MPU9150-4.dll");
-
-
-    QString qs_tmp;
-    qs_tmp = "Left thigh = ";qs_tmp.append(!b_lh? "OK":"Fail");
-    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
-    qs_tmp = "Left shank = ";qs_tmp.append(!b_lk? "OK":"Fail");
-    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
-    qs_tmp = "Right thigh = ";qs_tmp.append(!b_rh? "OK":"Fail");
-    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
-    qs_tmp = "Right thigh = ";qs_tmp.append(!b_rk? "OK":"Fail");
-    ui->textBrowser->append(qs_tmp);qs_tmp.clear();
-
-    if(!b_lh && !b_lk && !b_rh && !b_rk){
-        cout<<"ALL device OK"<<endl;
-        ui->textBrowser->append("ALL device OK");
-        timer_->start();
-    }
-
-    ui->pushButton_newFile->setEnabled(true);
 }
 
 void MainWindow::insertToTXT_Thread()
 {
     QFuture<void> future = QtConcurrent::run(this, &MainWindow::insertToTXT);
     future.waitForFinished();
-
-
-
-
-}
-
-void MainWindow::insertToTXT()
-{
-    if(b_saving_file)
-    {
-        if (file->open(QIODevice::WriteOnly | QIODevice::Text))
-            return;
-
-        QTextStream out(file);
-        //    out << "The magic number is: " << 49 << "\n";
-        for(int angle = 0; angle < 4; angle++)
-        {
-            out <<angle_data_buffer[angle]<<",";
-        }
-
-
-        for(int row = 0; row < row_Num; row++)
-            for(int col = 0; col < col_Num ; col++)
-            {
-
-                out <<data_buffer[row][col]<<",";
-
-            }
-
-        out <<endl;
-    }
 }
 
 void MainWindow::on_pushButton_newFile_clicked()
@@ -236,16 +198,15 @@ void MainWindow::on_pushButton_newFile_clicked()
     QTime time = QTime::currentTime();
     QString timeString = locale.toString(time, "hhmmss");
     QString username = ui->lineEdit_username->text();
-    QString filepath = "./DataXX/"+ username + "_" + dateString + "_" + timeString +".txt";
+    QString filepath = "./Data/"+ username + "_" + dateString + "_" + timeString +".txt";
     ui->textBrowser->append(filepath);
-
 
     if(timer_->isActive())
     {
         if(!QDir("Folder").exists())
-            QDir().mkdir("DataXX");
+            QDir().mkdir("Data");
         file = new QFile(filepath);
-//        timer_file->start();
+        //        timer_file->start();
         b_saving_file = true;
 
     }else
@@ -254,13 +215,62 @@ void MainWindow::on_pushButton_newFile_clicked()
 
     }
 
+
+
     ui->pushButton_newFile->setDisabled(true);
     ui->pushButton_saveFile->setEnabled(true);
 }
 
+void MainWindow::insertToTXT()
+{
+
+
+
+    if(b_saving_file)
+    {
+        if (file->open(QFile::WriteOnly | QIODevice::Text)) {
+            QTextStream out(file);
+            out << "The answer is " << 42;
+        }
+//        if (!file->open(QIODevice::WriteOnly | QIODevice::Append)){
+//        QTextStream out(file);
+//        if(!dataIn)
+//        {
+//            out <<"Left Hip"<<","
+//               <<"Right Hip"<<","
+//              <<"Left Knee"<<","
+//             <<"Right Knee"<<","
+//            <<"Back"<<" Roll"<<","<<"Back"<<" yaw"<<","<<"Back"<<" Pitch"<<","
+//            <<"Left Thigh"<<" Roll"<<","<<"Left Thigh"<<" yaw"<<","<<"Left Thigh"<<" Pitch"<<","
+//            <<"Right Thigh"<<" Roll"<<","<<"Right Thigh"<<" yaw"<<","<<"Right Thigh"<<" Pitch"<<","
+//            <<"Left Shank"<<" Roll"<<","<<"Left Shank"<<" yaw"<<","<<"Left Shank"<<" Pitch"<<","
+//            <<"Right Shank"<<" Roll"<<","<<"Right Shank"<<" yaw"<<","<<"Right Shank"<<" Pitch"<<endl;
+//            dataIn = true;
+
+//        }
+
+
+////        for(int angle = 0; angle < data_buffer_number; angle++)
+////        {
+////            out <<data_buffer[angle]<<",";
+////        }
+
+
+////        for(int angle = 0; angle < 4; angle++)
+////        {
+////            out <<data_buffer[angle]<<",";
+////        }
+
+
+//        out<<1<<endl;
+//        }
+    }
+}
+
 void MainWindow::on_pushButton_saveFile_clicked()
 {
-//    timer_file->stop();
+    //    timer_file->stop();
+    dataIn = false;
     b_saving_file = false;
     ui->textBrowser->append("File save");
     file->close();
